211c5546ddd139bc93f8fc2ba17810d3d04fed9a
jahrulezfrancis <longsaarmuknaan@gmail.com>
2022-09-27 13:58:08 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Posts } from './Postlist';
+
+
+function PostComp(props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>{props.body}</p>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {Posts.map((element, index) => {
+      return  <PostComp key={element.id} title={element.title} body={element.body} />
+     })}
     </div>
   );
 }
